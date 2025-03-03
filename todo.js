54a4d2f6d3e88013fe3a60cc6e9eec1eb011adaa
Arsalaan Ahmad <arsalaan745@gmail.com>
2025-03-03 20:28:47 +0530

    leos change

--- todo.js
+++ todo.js
@@ -1,7 +1,15 @@
 let todoList = [];
 
 while (true) {
-    let req = prompt("Please enter your request: ").toLowerCase();
+    let userInput = prompt("Please enter your request: ");
+
+    // Handle cancel or empty input
+    if (userInput === null) {
+        console.log("Exiting the To-Do app.....");
+        break;
+    }
+
+    let req = userInput.toLowerCase();
 
     switch (req) {
         case "add":
@@ -16,9 +24,8 @@ while (true) {
             }
             break;
 
-
         case "delete":
-            let taskIndex = parseInt(prompt("Enter the index of the task you wants to delete."));
+            let taskIndex = parseInt(prompt("Enter the index of the task you want to delete."));
             if (isNaN(taskIndex) || taskIndex < 1 || taskIndex > todoList.length) {
                 console.log("Please enter a valid Index!");
             }
@@ -27,7 +34,6 @@ while (true) {
                 console.log(`Task ${taskIndex} deleted successfully.`);
             }
             break;
-
 
         case "list":
             console.log("____________________");
@@ -43,14 +49,17 @@ while (true) {
             console.log("____________________")
             break;
 
-
         case "exit":
             console.log("Exiting the To-Do app.....");
+            // Use break to exit the loop
             break;
-
 
         default:
             console.log("Invalid request!");
+    }
 
+    // Add this condition to exit the loop when the user chooses "exit"
+    if (req === "exit") {
+        break;
     }
 }
